df605454ea0182c61667c0863587b39dac99419f
NCONTED <NCONTED@mynorth.mymdc.net>
2020-02-05 15:27:06 -0500

    do homework

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Jumbotron from "./components/Jumbotron";
+import "../src/styles/App.css";
 import Cards from "./components/Cards";
+import Card from "./components/Cards";
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
       <Navbar />
       <Jumbotron />
-      <Cards />
-    </div>
+      <div className="container">
+        <div className="row card-deck justify-content-md-center">
+          <div className="col col-md-4 col-lg-3 col-sm-6">
+            <Card />
+          </div>
+          <div className="col col-md-4 col-lg-3 col-sm-6">
+            <Card />
+          </div>
+          <div className="col col-md-4 col-lg-3 col-sm-6">
+            <Card />
+          </div>
+          <div className="col col-md-4 col-lg-3 col-sm-6">
+            <Card />
+          </div>
+        </div>
+      </div>
+
+    </React.Fragment>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,25 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav className="navbar navbar-dark bg-dark">
-        <div className="container">
-          <span className="navbar-brand mb-0 h1">Start Bootstrap</span>
-          <div className="row cols-4">
-            <div className="col">Home</div>
-            <div className="col">About</div>
-            <div className="col">Services</div>
-            <div className="col">Contact</div>
-          </div>
-        </div>
-      </nav>
-    </div>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <a className="navbar-brand" href="#">Navbar</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Features</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Pricing</a>
+          </li>
+        </ul>
+      </div>
+    </nav >
   );
 };
 
